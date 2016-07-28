2833b3bfb5d46b62600a1f94c2bab2d3354b429c
Hector Huertas <hectorhuertas@gmail.com>
2016-07-28 17:37:53 +0200

    Implement user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'login', to: 'sessions#new'
+  get  'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
 
   resources :users, only: [:create]
